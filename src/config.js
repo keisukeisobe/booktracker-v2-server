768dc3c0ee31a8e5515c36d76626e0cc4461402d
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2021-03-31 15:59:40 -0400

    scripting changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://booktracker@localhost/booktracker',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/booktracker2-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 };
